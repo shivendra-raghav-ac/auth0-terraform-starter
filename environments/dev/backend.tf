1d1f27ccfcffead69cac7c9bf2199c05fd1753b3
Shivendra <shivendra.raghav@outlook.com>
2025-10-13 22:06:47 -0500

    Backend and tfvars json commit

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,12 +1,11 @@
 # ========================================
 # Terraform Backend Configuration (DEV)
 # ========================================
-# Remote state storage in S3 with DynamoDB locking
 # Update bucket name and KMS key before use
 
 terraform {
   backend "s3" {
-    bucket  = "auth0-terraform-state-1760024845"
+    bucket  = "auth0-terraform-state"
     key     = "auth0/dev/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
