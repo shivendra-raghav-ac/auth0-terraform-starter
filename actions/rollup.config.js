7bb8525b9014ba81cf2b702ddc08b5c509338e77
Shivendra <shivendra.raghav@outlook.com>
2025-11-03 20:21:05 +0530

    Actions Changes Upgrade

--- actions/rollup.config.js
+++ actions/rollup.config.js
@@ -1,14 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: ["src/post-login-action.ts"],
+  input: "src/post-login-action.ts",
   output: {
     strict: false,
     format: "cjs",
-    dir: "dist"
+    dir: "dist",
+    sourcemap: true
   },
   external: [],
   plugins: [
-    typescript({ module: 'es6' })
+    typescript({
+      module: 'esnext',
+      tsconfig: './tsconfig.json'
+    })
   ]
 };
